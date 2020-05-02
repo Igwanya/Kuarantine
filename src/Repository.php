03c3b60b9f7c13f8e0db2c115eb8ec7ac0ba7e26
Igwanya <felixmuthui32@gmail.com>
2020-05-03 01:24:00 +0300

    Modified the repository code to reflect cat2 database changes

--- src/Repository.php
+++ src/Repository.php
@@ -179,33 +179,39 @@ class Repository
     }
 
     /**
-     * Create a new posts.
-     * @param $title
-     * @param $body
-     * @param $author
+     * @param $headline
+     * @param $content
+     * @param $userID
      * @return array
      */
-    public function add_post($title, $body, $author)
+    public function add_post($headline, $content, $userID)
     {
         $result = array(
-            "message" => "",
+            "status"  => "",
+            "body"    => array(),
             "error"   => ""
         );
-        $insert_stmt = "INSERT INTO posts (title, body, author) VALUES (?,?,?)";
+        $insert_stmt = "INSERT INTO articles (headline, content, created, userID) VALUES (?,?,?,?)";
         /* Prepared statement, stage 1: prepare */
         if (!($stmt = $this->db->prepare($insert_stmt))) {
-            trigger_error("Prepare failed: (" . $this->db->errno . ") " . $this->db->error);
+            trigger_error("Prepare failed: (" . $this->db->errno . ") "
+                . $this->db->error);
         }
+        $created = date("Y-m-d");
         /* Prepared statement, stage 2: bind and execute */
-        if (!$stmt->bind_param("sss", $title, $body, $author)) {
-            trigger_error("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+        if (!$stmt->bind_param("sssi", $headline, $content, $created, $userID)) {
+            trigger_error("Binding parameters failed: (" . $stmt->errno . ") "
+                . $stmt->error);
         }
         if (!$stmt->execute()) {
-            trigger_error("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-            $result["message"] = null;
-            $result["error"] = $_SERVER["SERVER_PROTOCOL"]." 404 Not Found";
+            trigger_error("Execute failed: (" . $stmt->errno . ") " .
+                $stmt->error);
+            $result["status"] = "Article not posted";
+            $result["error"] =  $stmt->error;
         } else {
-            $result["message"] = "Post inserted successfully";
+            $result["status"] = "Article inserted successfully";
+//            TODO:: decide whether to return the posted article
+            $result["body"] = null;
             $result["error"]   = null;
             return $result;
         }
@@ -213,43 +219,55 @@ class Repository
     }
 
     /**
-     * @param $id
+     * @param $articleID
      * @return array
      */
-    public function read_post($id){
+    public function read_post($articleID){
         $result = array(
-            "message"  => array(
-                "id"         => 0,
-                "title"      => "",
-                "body"       => "",
-                "author"     => "",
-                "created_at" => ""
+            "status"   => "" ,
+            "body"  => array(
+                "articleID"         => 0,
+                "headline"          => "",
+                "content"           => "",
+                "userID"            => "",
+                "created"           => "",
+                "lastUpdated"       => ""
             ),
             "error"    => ""
         );
-        $sql =  "SELECT * FROM posts WHERE id=?";
+        
+        $sql =  "SELECT * FROM articles WHERE articleID LIKE ?";
         $stmt = $this->db->prepare($sql);
-        $stmt->bind_param('i', $id);
+        $articleID = filter_var($articleID, FILTER_VALIDATE_INT);
+        /* Prepared statement, stage 2: bind and execute */
+        if (!$stmt->bind_param("i",$articleID)) {
+            trigger_error("Binding parameters failed: ("
+                . $stmt->errno . ") " . $stmt->error);
+            $result['error'] =  $stmt->error;
+        }
         if (!$stmt->execute()) {
             trigger_error("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-            $result["message"] = null;
-            $result["error"] = $_SERVER["SERVER_PROTOCOL"]." 404 Not Found";
+            $result['status']  = "Article read unsuccessful";
+            $result["body"] = null;
+            $result["error"] = $stmt->error;
         } else {
             $res = $stmt->get_result();
             $row = $res->fetch_assoc();
-            if ($row['id'] != null){
-                $result["message"]['id']         =  $row['id'];
-                $result["message"]['title']      =  $row['title'];
-                $result["message"]['body']       =  $row['body'];
-                $result["message"]['author']     =  $row['author'];
-                $result["message"]['created_at'] =  $row['created_at'];
+            if ($row['articleID'] != null){
+                $result["body"]['articleID']   =  $row['articleID'];
+                $result["body"]['headline']    =  $row['headline'];
+                $result["body"]['content']     =  $row['content'];
+                $result["body"]['userID']      =  $row['userID'];
+                $result["body"]['created']     =  $row['created'];
+                $result["body"]['lastUpdated'] =  $row['lastUpdated'];
+                $result['status']  = "Article read successfully";
                 $result["error"]   = null;
                 return $result;
             } else {
                 // TODO:: Be careful,  call from api request ONLY!!!
                 header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
-                $result["message"] = null;
-                $result["error"] = "No post found";
+                $result["body"] = null;
+                $result["error"] = "No Article found";
             }
         }
         return $result;
@@ -262,49 +280,55 @@ class Repository
      public function read_all_posts()
      {
          $result = array(
-             "message"  => array(
-                 "id"         => 0,
-                 "title"      => "",
-                 "body"       => "",
-                 "author"     => "",
-                 "created_at" => ""
+             "status"   => "" ,
+             "body"  => array(
+                 "articleID"         => 0,
+                 "headline"          => "",
+                 "content"           => "",
+                 "userID"            => "",
+                 "created"           => "",
+                 "lastUpdated"       => ""
              ),
              "error"    => ""
          );
-         $sql = "SELECT * FROM posts";
+         $sql = "SELECT * FROM articles";
          $stmt = $this->db->prepare($sql);
          $stmt->execute();
          $res = $stmt->get_result();
-
          if ($res->num_rows > 0){
+             $result['status'] = "Article read successful";
              while($row = $res->fetch_assoc()){
                  $post_data = [
-                     'id'         => $row['id'],
-                     'title'      => $row['title'],
-                     'body'       => html_entity_decode($row['body']),
-                     'author'     => $row['author'],
-                     'created_at' => $row['created_at']
+                     'articleID'   => $row['articleID'],
+                     'headline'    => $row['headline'],
+                     'content'     => html_entity_decode($row['content']),
+                     'userID'      => $row['userID'],
+                     'created'     => $row['created'],
+                     'lastUpdated' => $row['lastUpdated']
                  ];
-                 array_push($result['message'], $post_data);
+                 array_push($result['body'], $post_data);
              }
          } else {
-             $result['message'] = null;
-             $result['error'] = "No posts yet! ";
+             $result['body'] = null;
+             $result['error'] = "No Articles yet! ";
          }
          return $result;
      }
 
+
     /**
-     * @param $id
-     * @param $title
-     * @param $body
+     * @param $articleID
+     * @param $headline
+     * @param $content
+     * @param $userID
      * @return array
      */
-   public function update_post($id, $title, $body)
+   public function update_post($articleID, $headline, $content, $userID)
    {
-       $sql = "UPDATE posts SET title=?, body=? WHERE id=?";
+       $sql = "UPDATE articles SET headline=?, content=?, userID=?, lastUpdated=? WHERE articleID=?";
        $result = array(
-           "message" => "",
+           "status" => "",
+           "body" => array(),
            "error"   => ""
        );
        /* Prepared statement, stage 1: prepare */
@@ -312,44 +336,250 @@ class Repository
            trigger_error("Prepare failed: (" . $this->db->errno . ") " . $this->db->error);
        }
        /* Prepared statement, stage 2: bind and execute */
-       if (!$stmt->bind_param("ssi", $title, $body, $id)) {
+       // TODO:: Squash this bug !!!
+       // TODO:: the date does not reflect in the table
+       $lastUpdated = date("Y-m-d");
+       if (!$stmt->bind_param("sssss", $headline,$content, $userID, $lastUpdated, $articleID)) {
            trigger_error("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
        }
        if (!$stmt->execute()) {
            trigger_error("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-           $result["message"] = null;
-           $result["error"] = "Post not updated. Invalid ID.";
+           $result["status"] = "No article updated ...";
+           $result['body']  = null;
+           $result["error"] =  $stmt->error;
            return $result;
+       } else{
+           $result["status"] = "Article updated successfully.";
+           $result["body"]   = $this->read_post($articleID)['body'];
+           $result["error"]   = null;
        }
-       $result["message"] = "Post updated successfully.";
-       $result["error"]   = null;
        return $result;
    }
 
-   public function delete_post($post_id)
+    /**
+     * TODO:: userID is foreign key to articles table   RESEARCH MORE!!
+     * @param $articleID
+     * @return array
+     */
+   public function delete_post($articleID)
    {
-       $sql = "DELETE FROM posts WHERE id=?";
+       $sql = "DELETE FROM articles WHERE articleID=?";
        $result = array(
-           "message" => "",
+           "status" => "",
+           "body" => array(),
            "error"   => ""
        );
-       /* Prepared statement, stage 1: prepare */
+       /*epared statement, stage 1: prepare */
        if (!($stmt = $this->db->prepare($sql))) {
            trigger_error("Prepare failed: (" . $this->db->errno . ") " . $this->db->error);
        }
        /* Prepared statement, stage 2: bind and execute */
-       if (!$stmt->bind_param("i", $post_id)) {
+       if (!$stmt->bind_param("i", $articleID)) {
            trigger_error("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
        }
        if (!$stmt->execute()) {
            trigger_error("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-           $result["message"] = null;
-           $result["error"] = "Post not deleted Invalid ID.";
+           $result["body"]   = null;
+           $result["status"] = "Article not deleted Invalid ID.";
+           $result['error']  =  $stmt->error;
            return $result;
        }
-       $result["message"] = "Post deleted successfully.";
+       $result["status"] = "Article deleted successfully.";
+       $result["body"]   = null;
        $result["error"]   = null;
        return $result;
    }
 
+    /**
+     * @param $app_version
+     * @param $model
+     * @param $user
+     * @param $api_level
+     * @param $screen_resolution
+     * @param $screen_density
+     * @return array
+     */
+   public function insert_app_data($app_version, $model, $user,
+                                   $api_level, $screen_resolution, $screen_density) {
+       $result = array(
+           "status" => "" ,
+           "body" => "",
+           "error"   => ""
+       );
+       $insert_stmt = "INSERT INTO app (app_version, model, user, api_level,
+                 screen_resolution, screen_density) VALUES (?,?,?,?,?,?)";
+       /* Prepared statement, stage 1: prepare */
+       if (!($stmt = $this->db->prepare($insert_stmt))) {
+           trigger_error("Prepare failed: (" . $this->db->errno . ") " . $this->db->error);
+       }
+       /* Prepared statement, stage 2: bind and execute */
+       if (!$stmt->bind_param("ssssss", $app_version, $model, $user,
+           $api_level, $screen_resolution,$screen_density)) {
+           trigger_error("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+       }
+       if (!$stmt->execute()) {
+           trigger_error("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+           $result["body"] = null;
+           $result["error"] = $stmt->error;
+       } else {
+           $result["status"] = "Data inserted successfully";
+           $result["body"] = null;
+           $result["error"]   = null;
+           return $result;
+       }
+       return $result;
+   }
+
+    /**
+     * @return array
+     */
+   public function read_app_data(){
+       $result = array(
+           "status" => "" ,
+           "body" => array(),
+           "error"   => ""
+       );
+       $sql = "SELECT * FROM app";
+       $stmt = $this->db->prepare($sql);
+       $stmt->execute();
+       $res = $stmt->get_result();
+       if ($res->num_rows > 0){
+           $result['status'] = "Reading app data ...";
+           $result['error'] = null;
+           while($row = $res->fetch_assoc()){
+               $data = [
+                   "appID"             => $row['appID'],
+	               "app_version"       => $row['app_version'],
+                   "model"             => $row['model'],
+                   "user"              => $row['user'],
+                   "api_level"         => $row['api_level'],
+                   "screen_resolution" => $row['screen_resolution'],
+                   "screen_density"    => $row['screen_density']
+               ];
+               array_push($result['body'], $data);
+           }
+       } else {
+           $result['body'] = null;
+           $result['error'] = "Application data not present ... ";
+       }
+       return $result;
+   }
+
+   public function read_app_data_by_id($appID){
+       $result = array(
+           "status" => "" ,
+           "body" => array(),
+           "error"   => ""
+       );
+       $sql = "SELECT * FROM app WHERE appID LIKE ?";
+       $stmt = $this->db->prepare($sql);
+       /* Prepared statement, stage 2: bind and execute */
+       if (!$stmt->bind_param("i",$appID)) {
+           trigger_error("Binding parameters failed: ("
+               . $stmt->errno . ") " . $stmt->error);
+           $result['error'] =  $stmt->error;
+       }
+       $stmt->execute();
+       $res = $stmt->get_result();
+       if ($res->num_rows > 0){
+           $result['status'] = "Reading app data ...";
+           $result['error'] = null;
+           $row = $res->fetch_assoc();
+           $result['body'] = [
+               "appID"             => $row['appID'],
+               "app_version"       => $row['app_version'],
+               "model"             => $row['model'],
+               "user"              => $row['user'],
+               "api_level"         => $row['api_level'],
+               "screen_resolution" => $row['screen_resolution'],
+               "screen_density"    => $row['screen_density']
+           ];
+       } else {
+           $result['body'] = null;
+           $result['status'] = "Application data not present ... ";
+           $result['error']  = "Invalid Application id";
+       }
+       return $result;
+   }
+
+    /**
+     * @param $appID
+     * @param $app_version
+     * @param $model
+     * @param $user
+     * @param $api_level
+     * @param $screen_resolution
+     * @param $screen_density
+     * @return array
+     */
+   public function update_app_data($appID, $app_version, $model, $user,
+                                   $api_level, $screen_resolution, $screen_density){
+       $sql = "UPDATE app SET app_version=?, model=?, user=?,
+                  api_level=?, screen_resolution=?, screen_density=? WHERE appID=?";
+       $result = array(
+           "status" => "",
+           "body" => array(),
+           "error"   => ""
+       );
+       /* Prepared statement, stage 1: prepare */
+       if (!($stmt = $this->db->prepare($sql))) {
+           trigger_error("Prepare failed: (" . $this->db->errno . ") " .
+               $this->db->error);
+       }
+       /* Prepared statement, stage 2: bind and execute */
+       if (!$stmt->bind_param("ssssssi",$app_version, $model, $user,
+           $api_level, $screen_resolution, $screen_density,$appID)) {
+           trigger_error("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+       }
+       if (!$stmt->execute()) {
+           trigger_error("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+           $result["body"] = null;
+           $result["status"] = "App data not updated. Invalid ID.";
+           $result['error']  = $stmt->error;
+           return $result;
+       } else {
+           $result["status"] = "App data updated successfully.";
+           $result["body"]   = $this->read_app_data_by_id($appID)['body'];
+           $result['error']  = null;
+       }
+
+       return $result;
+   }
+
+    /**
+     * @param $appID
+     * @return array
+     */
+   public function delete_app_data($appID)
+   {
+       $sql = "DELETE FROM app WHERE appID=?";
+       $result = array(
+           "status"  => "",
+           "body" => "",
+           "error"   => ""
+       );
+       /* Prepared statement, stage 1: prepare */
+       if (!($stmt = $this->db->prepare($sql))) {
+           trigger_error("Prepare failed: (" .
+               $this->db->errno . ") " . $this->db->error);
+       }
+       /* Prepared statement, stage 2: bind and execute */
+       if (!$stmt->bind_param("i", $appID)) {
+           trigger_error("Binding parameters failed: (" .
+               $stmt->errno . ") " . $stmt->error);
+       }
+       if (!$stmt->execute()) {
+           trigger_error("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+           $result['status'] = "App data not deleted Invalid ID.";
+           $result["body"] = null;
+           $result["error"] = $stmt->error;
+           return $result;
+       }
+       $result["status"] = "App data deleted successfully.";
+       $result['body']   = null;
+       $result["error"]   = null;
+       return $result;
+
+   }
+   
 }
