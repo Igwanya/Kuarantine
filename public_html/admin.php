3da3249d4151ebb350846bd9351a13a17de39828
Igwanya <felixmuthui32@gmail.com>
2020-05-04 02:03:34 +0300

    Initial commit

---
+++ public_html/admin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MUTHUI
+ * Date: 03-May-20
+ * Time: 6:29 PM
+ */
+namespace Src;
+
+require_once __DIR__ . '../../vendor/autoload.php';
+ session_start();
