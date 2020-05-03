d5dd627e166d30c0bce11c9c803556f7f3e70329
Igwanya <felixmuthui32@gmail.com>
2020-05-04 01:59:29 +0300

    Modified links the outlook of the page

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (isset($_COOKIE['count'])) {
 setcookie('count', $count, time()+3600);
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-
+//$_SERVER["is_authenticated"] = true;
 ?>
 <!doctype html>
 <html lang="en">
@@ -32,136 +32,156 @@ $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
     <link href="public_html/res/vendor/bootstrap/css/bootstrap.css" rel="stylesheet"  media="screen,projection">
     <link href="public_html/res/vendor/materialize/css/materialize.css" rel="stylesheet"  media="screen,projection">
     <link href="public_html/res/css/main.css" rel="stylesheet"  media="screen,projection">
-    <title>
-        <?php
-        echo $host;
-        ?>
-    </title>
+    <title><?php echo $host;?></title>
 </head>
 <body>
-
 <nav class="white" role="navigation" id="indexNav">
     <div class="nav-wrapper container">
         <a id="logo-container" href="/" class="brand-logo">Logo</a>
         <ul class="right hide-on-med-and-down" id="navList">
+            <li><a href="#" id="indexNavLinkFeatured">Featured</a></li>
+            <li><a href="#" id="indexNavLinkProducts">Products</a></li>
+            <li><a href="#" id="indexNavLinkArticles">Articles</a></li>
             <li><a href="#" id="indexNavLinkAbout">About</a></li>
             <li><a href="#" id="indexNavLinkContact">Contact</a></li>
-            <li><a href="public_html/login.php" id="indexLinkLogin">Login</a></li>
-            <li><a href="public_html/register.php" id="indexLinkRegister">Register</a></li>
+            <?php if ( isset($_SERVER["is_authenticated"])) {?>
+                <?php if ( !$_SERVER["is_authenticated"]){ ?>
+                    <li><a href="public_html/login.php" id="indexLinkLogin">Login</a></li>
+                    <li><a href="public_html/register.php" id="indexLinkRegister">Register</a></li>
+                <?php } else { ?>
+                    <li>
+                        <ul>
+                            <li><a href="public_html/profile.php" id="indexLinkProfile"><i class="material-icons">account_circle</i></a></li>
+                            <li><a href="public_html/profile.php" id="indexLinkProfile">My Account</a></li>
+                        </ul>
+                    </li>
+                <?php } ?>
+            <?php } else { ?>
+                <li><a href="public_html/login.php" id="indexLinkLogin">Login</a></li>
+                <li><a href="public_html/register.php" id="indexLinkRegister">Register</a></li>
+            <?php } ?>
         </ul>
         <ul id="nav-mobile" class="sidenav">
-            <li><a href="#">Navbar Link</a></li>
+            <li><a href="#featured" id="indexNavLinkFeatured">Featured</a></li>
+            <li><a href="#" id="indexNavLinkProducts">Products</a></li>
+            <li><a href="#" id="indexNavLinkArticles">Articles</a></li>
+            <li><a href="#" id="indexNavLinkAbout">About</a></li>
+            <li><a href="#" id="indexNavLinkContact">Contact</a></li>
+            <?php if ( isset($_SERVER["is_authenticated"])) {?>
+                <?php if ( !$_SERVER["is_authenticated"]){ ?>
+                    <li><a href="public_html/login.php" id="indexLinkLogin">Login</a></li>
+                    <li><a href="public_html/register.php" id="indexLinkRegister">Register</a></li>
+                <?php } else { ?>
+                    <li>
+                        <ul>
+                            <li><a href="public_html/profile.php" id="indexLinkProfile"><i class="material-icons">account_circle</i></a></li>
+                            <li><a href="public_html/profile.php" id="indexLinkProfile">My Account</a></li>
+                        </ul>
+                    </li>
+                <?php } ?>
+            <?php } else { ?>
+            <li><a href="public_html/login.php" id="indexLinkLogin">Login</a></li>
+            <li><a href="public_html/register.php" id="indexLinkRegister">Register</a></li>
+            <?php } ?>
         </ul>
         <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
 </nav>
-
-
 <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
         <div class="container">
             <br><br>
-            <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
+            <h1 class="header center teal-text text-lighten-2">KUARANTINE</h1>
             <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <h5 class="header col s12 light">A modern responsive site for all your quarantine manennos.</h5>
             </div>
             <div class="row center">
-                <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+<!--                TODO:: implement the download feature-->
+                <a href="" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
             </div>
             <br><br>
-
         </div>
     </div>
-    <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
+    <div class="parallax"><img src="public_html/res/img/background1.jpg" alt="Unsplashed background img 1"></div>
 </div>
-
-
-
 <div class="container">
     <div class="section">
-
         <!--   Icon Section   -->
         <div class="row">
             <div class="col s12 m4">
                 <div class="icon-block">
-                    <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-                    <h5 class="center">Speeds up development</h5>
-
-                    <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                    <h2 class="center brown-text"><i class="material-icons">add_shopping_cart</i></h2>
+                    <h5 class="center">Products</h5>
+                    <p class="light">We did most of the heavy lifting for you to provide access to merchants that are ready to sell their products. Anything that you could possible need when in quarantine we have it for you on the platform</p>
                 </div>
             </div>
-
             <div class="col s12 m4">
                 <div class="icon-block">
                     <h2 class="center brown-text"><i class="material-icons">group</i></h2>
                     <h5 class="center">User Experience Focused</h5>
-
-                    <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                    <p class="light">By utilizing elements and principles of Material Design, we were able to create an application that incorporates components and animations that provide more feedback to users.
+                        Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
                 </div>
             </div>
-
             <div class="col s12 m4">
                 <div class="icon-block">
                     <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-                    <h5 class="center">Easy to work with</h5>
-
-                    <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                    <h5 class="center">Articles</h5>
+                    <p class="light">We have provided detailed articles as well as user shared on the COVID-19 virus tips to help get started. We are also always open to feedback and can answer any questions a user may have about the quarantine situation.</p>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
-
-
-<div class="parallax-container valign-wrapper">
+<div class="parallax-container valign-wrapper" id="featured">
     <div class="section no-pad-bot">
         <div class="container">
             <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <div class="col s12">
+                    <h2 class="header light text-center">Featured</h2>
+                    <h5 class="header light">A curated list of new products that are being sold by merchants.</h5>
+                </div>
+            </div>
+            <div class="row center">
+                <!--                TODO:: implement the download feature-->
+                <a href="" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Explore</a>
             </div>
         </div>
     </div>
-    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
+    <div class="parallax"><img src="public_html/res/img/background2.jpg" alt="Unsplashed background img 2"></div>
 </div>
-
-
 <!--Contact section-->
 <div class="container">
  <div class="section" id="contact">
   <div class="row">
    <div class="col s12 center">
        <h3><i class="mdi-content-send brown-text"></i></h3>
-        <h4>Contact us</h4>
-        <form class="col s12" method="post" action="">
-            <div class="row">
-                <div class="input-field">
-                    <input id="inputEmail" type="email" class="validate">
-                    <label for="inputEmail">Email</label>
-                    <span class="helper-text" data-error="wrong" data-success="right">Required*</span>
-                </div>
-                <div class="input-field col s12">
-                    <textarea id="textarea1" class="materialize-textarea"></textarea>
-                    <label for="textarea1">Textarea</label>
-                </div>
-            </div>
-        </form>
+        <h4>About</h4>
+         <p class="text-body">We are a team of college students working on this project like
+             it's our full time job. This project is done inorder to complete Mobile application development CAT2 assignment. We have provided detailed articles as well as user shared
+             on the COVID-19 virus tips to help get started. We are also always open to feedback and can answer any questions a user may have about the quarantine situation.</p>
    </div>
   </div>
  </div>
 </div>
 
-
-
 <div class="parallax-container valign-wrapper">
     <div class="section no-pad-bot">
         <div class="container">
             <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <div class="col s12">
+                    <h2 class="header light text-center">Articles</h2>
+                    <h5 class="header light">Shared posts on COVID-19 status and tips on the quarantine.</h5>
+                </div>
+
+            </div>
+            <div class="row center">
+                <!--                TODO:: implement this feature-->
+                <a href="" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Read & share</a>
             </div>
         </div>
     </div>
-    <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+    <div class="parallax"><img src="public_html/res/img/background3.jpg" alt="Unsplashed background img 3"></div>
 </div>
 
 <footer class="page-footer teal">
@@ -169,14 +189,12 @@ $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
         <div class="row">
             <div class="col l6 s12">
                 <h5 class="white-text">Company Bio</h5>
-                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
+                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. This project is done inorder to complete Mobile application development CAT2 assignment.</p>
             </div>
             <div class="col l3 s12">
                 <h5 class="white-text">Settings</h5>
                 <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="public_html/admin.php">Administrator</a></li>
                     <li><a class="white-text" href="#!">Link 2</a></li>
                     <li><a class="white-text" href="#!">Link 3</a></li>
                     <li><a class="white-text" href="#!">Link 4</a></li>
@@ -185,21 +203,20 @@ $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
             <div class="col l3 s12">
                 <h5 class="white-text">Connect</h5>
                 <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
+                    <li><a class="white-text" href="#!">Facebook</a></li>
+                    <li><a class="white-text" href="#!">Twitter</a></li>
+                    <li><a class="white-text" href="http://www.github.com/Igwanya">GITHUB</a></li>
+                    <li><a class="white-text" href="#!">felixmuthui32@gmail.com</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright">
         <div class="container">
-            Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+            Made by <a class="brown-text text-lighten-3" href="#!">SCT221-0788/2016</a>
         </div>
     </div>
 </footer>
-
 <!--  Scripts-->
 <script src="public_html/res/vendor/jquery-3.4.1.js"></script>
 <script src="public_html/res/vendor/popper.min.js"></script>
