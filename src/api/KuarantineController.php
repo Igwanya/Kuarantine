5bdc3ab0a6fa3cdd3cb287a33e23f83a5eb0af66
Igwanya <felixmuthui32@gmail.com>
2020-05-12 21:28:13 +0300

    Rewrote the app table fields

--- src/api/KuarantineController.php
+++ src/api/KuarantineController.php
@@ -36,19 +36,35 @@ switch ($requestMethod){
 
         break;
     case 'POST':
+        $result = array(
+            "status"  => "" ,
+            "body"    => array(),
+            "error"   => array()
+        );
         $repository = new Repository();
         $data = json_decode(file_get_contents("php://input"));
-        if (
-            isset($data->app_version) && isset($data->model) && isset($data->user)
-            && isset($data->api_level) && isset($data->screen_resolution) && isset($data->screen_density)
-        ){
-            echo json_encode($repository->insert_app_data($data->app_version, $data->model,
-                $data->user, $data->api_level, $data->screen_resolution, $data->screen_density));
-
-        } else{
+        /**
+         * Expected inputs:
+         *      (
+         *          [application_id] =>
+         *          [created]        =>
+         *          [display]        =>
+         *          [user_id]        =>
+         *          [version_name]   =>
+         *          [version_code]   =>
+         *   )
+         */
+        if (isset($data->application_id) && isset($data->created) && isset($data->display)
+            &&  isset($data->user_id) && isset($data->version_name) && isset($data->version_code) ) {
+            echo json_encode($repository->insert_app_data($data->application_id, $data->created,
+                $data->display, $data->user_id, $data->version_name, $data->version_code));
+        } else {
             http_response_code(404);
+           $result["status"] = "The required fields are empty | application_id | created | display 
+                                    | user_id | version_name |  version_code  ";
+           echo  json_encode($result);
         }
-        break;
+            break;
     case 'PUT':
         $repository = new Repository();
         $data = json_decode(file_get_contents("php://input"));
