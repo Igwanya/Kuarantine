6ff1e66c72b6137a7623193e7a3a85910e6bd2c5
Igwanya <felixmuthui32@gmail.com>
2020-05-16 06:05:26 +0300

    Initial commit

--- public_html/featured.php
+++ public_html/featured.php
@@ -5,4 +5,32 @@
  * Date: 16-May-20
  * Time: 5:27 AM
  */
+
 require_once __DIR__ . '../../vendor/autoload.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="felixmuthui32@gmail.com">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <link href="res/vendor/bootstrap/css/bootstrap.css" rel="stylesheet"  media="screen,projection">
+    <link href="res/vendor/materialize/css/materialize.css" rel="stylesheet"  media="screen,projection">
+    <link href="res/css/main.css" rel="stylesheet"  media="screen,projection">
+    <title>Products</title>
+</head>
+<body>
+
+<script type="text/javascript" src="res/vendor/jquery-3.4.1.js"></script>
+<script type="text/javascript" src="res/vendor/popper.min.js"></script>
+<script type="text/javascript" src="res/vendor/jquery.mobile-1.4.5.js"></script>
+<script type="text/javascript" src="res/vendor/materialize/js/materialize.js"></script>
+<script type="text/javascript" src="res/js/init.js"></script>
+</body>
+</html>
