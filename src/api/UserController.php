3f54f659af0dba88c494eee61db3470dfb599e32
Igwanya <felixmuthui32@gmail.com>
2020-06-02 15:43:13 +0300

    Added the user flow request response from api calls

--- src/api/UserController.php
+++ src/api/UserController.php
@@ -8,6 +8,7 @@
 
 namespace Src\api;
 
+use http\Exception\RuntimeException;
 use Src\auth\Login;
 use Src\auth\Register;
 use Src\models\User;
@@ -43,41 +44,50 @@ switch ($requestMethod) {
             $result['status'] = $db_query['status'];
             $result['body'] = $db_query['body'];
             $result['error'] = $db_query['error'];
+            http_response_code(200);
             echo json_encode($result);
         } else {
             $db_query = $repository->find_all_users();
             $result['body']   = $db_query['body'];
             $result['error']  = $db_query['error'];
+            http_response_code(200);
             echo json_encode($result);
+        }
+        if (!empty($result['error'])) {
+            http_response_code(404);
         }
         break;
     case 'POST':
         $result = array(
             "status" => "",
+            "path"  => "",
             "body"   => array(),
             "error"  => array(
-                "username_error" => "",
-                "email_error"    => ""
+                "query_error" => null,
             )
         );
+        $result['path']  = \Src\get_server_url_domain_name().$_SERVER['REQUEST_URI'];
         // GET DATA FORM REQUEST
         $data = json_decode(file_get_contents("php://input"));
-        if (!empty($data)) {
-            $register = new Register();
-            $register->setUsername($data->username);
-            $register->setEmail($data->email);
-            $result["error"]["username_error"] = $register->perform_username_check();
-            $result["error"]["email_error"]  = $register->perform_email_check();
-            $register->setPassword($data->password);
-            $result['error']  = array();
-            if ( empty($result["error"]["username_error"])
-                && empty($result["error"]["email_error"])){
-               $result = $register->register_user();
-            }
+        if (isset($data->id) && isset($data->username) && isset($data->url) && isset($data->email) && isset($data->first_name)
+        && isset($data->last_name) && isset($data->full_name) && isset($data->bio)) {
+            $db_query = $repository->find_user_with_id($data->id);
+             if ($db_query['body']['user'] != null)  {
+                 $result['status'] = $repository->update_user_to_db($data->id, $data->url, $data->username, $data->email, $data->first_name, $data->last_name, $data->full_name, $data->bio)['status'];
+                 $result['body'] = $repository->find_user_with_id($data->id)['body'];
+                 http_response_code(200);
+                 echo json_encode($result);
+             } else {
+                 $result['status']  = "Update failed";
+                 $result['error']  = $db_query['error'];
+                 http_response_code(404);
+                 echo json_encode($result);
+                 exit(0);
+             }
+        } else {
+            $result['status'] = "Expected fields | id | username | url | email | first_name | last_name | full_name | bio ";
+            http_response_code(404);
+            echo json_encode($result);
         }
-        echo json_encode($result);
-        break;
-    default:
-
         break;
 }
